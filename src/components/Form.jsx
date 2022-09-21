d03879a0bb5024d921afeb335a55b5bbb2d53368
Mylene Vinci <mylene.vinci@hotmail.com>
2022-09-21 16:11:02 +0200

    no location.reload

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -24,7 +24,6 @@ const Form = () => {
         .then(
             () => {
                 alert('Message envoyé')
-                window.location.reload()
             },
             () => {
                 alert('Message non envoyé veuillez réessayer')
