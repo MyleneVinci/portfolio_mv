3714aaeca7aa544eaa89efe1f71e0ac7498c739b
Mylene Vinci <mylene.vinci@hotmail.com>
2023-03-09 14:16:59 +0100

    add a new project

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,5 +1,6 @@
 import saintstagramMockup from '../assets/saintstagramMockup.png';
 import eurhekaMockup from '../assets/eurhekaMockup.jpg';
+import loststuffmockup from '../assets/loststuffmockup.jpg';
 import rock_paper_scissors_lizard_spockMockup from '../assets/rock_paper_scissors_lizard_spockMockup.jpg';
 import javascript from '../assets/javascript.png';
 import css from '../assets/css.png';
@@ -16,7 +17,6 @@ const ProjectCardData = [
         title: "Saintstagram",
         context:"projet réalisé en équipe dans le cadre de ma formation à la Wild Code School",
         resume: "réseau social fictif des chevaliers du zodiaque basé sur instagram",
-        duration: "réalisé en 4 semaines",
         techno1: html ,
         techno2:css,
         techno3:javascript,
@@ -32,7 +32,6 @@ const ProjectCardData = [
         title: "Eurheka!",
         context:"projet réalisé en équipe en partenariat avec un professionnel dans le cadre de ma formation à la Wild Code School",
         resume: "plateforme de consulting RH",
-        duration: "réalisé en 4 semaines",
         techno1: html ,
         techno2:css,
         techno3:javascript,
@@ -48,7 +47,6 @@ const ProjectCardData = [
         title: "Pierre Feuille Ciseaux Lézard Spock",
         context:"Projet Personnel",
         resume: "Variante du Pierre Feuille Ciseaux, popularisée par la série The Big Bang Theory",
-        duration: "réalisé en 4 heures",
         techno1: html ,
         techno2:css,
         techno3:javascript,
@@ -57,6 +55,21 @@ const ProjectCardData = [
         techno6: null,
         view: "https://the-big-bang-theory-game.netlify.app/",
         source: "https://github.com/MyleneVinci/rock-paper-scissors-lizard-spock"
+    },
+    {
+        id:4,
+        imgsrc: loststuffmockup,
+        title: "Objets perdus trouvés",
+        context:"Projet Personnel",
+        resume: "Platerforme permettant de déclarer des objets perdus ou trouvés et de les localiser sur une carte",
+        techno1: html ,
+        techno2:css,
+        techno3:javascript,
+        techno4: react,
+        techno5: node,
+        techno6: express,
+        view: null,
+        source: "https://github.com/MyleneVinci/my_app_lost_and_found_front"
     }
 ]
 
